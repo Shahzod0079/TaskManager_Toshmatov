a8555e360919202a018236639bfbba6b1341f799
Shahzod0079 <toshmatovshahzod69@gmail.com>
2026-03-14 15:00:18 +0500

    Проработанная структура и реализованное главное окно. Тошматов

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,13 @@ namespace TaskManager_Toshmatov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            DataContext = new VM_Pages();
         }
     }
 }
