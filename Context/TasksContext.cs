b2b1669965d99a2ed14a663eb040c62054dc17c8
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-15 12:38:19 +0500

    Разработанный контекст данных для модели задач. Тошматов

---
+++ Context/TasksContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskManager_Toshmatov.Modell;
+
+namespace TaskManager_Toshmatov.Context
+{
+    internal class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.connection, Config.Version);
+
+    }
+}
