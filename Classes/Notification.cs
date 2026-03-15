b860fcac0f0ad5458854dbd038446196978f9bb8
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-15 11:36:04 +0500

    Разработанный классы общего-пользования.  Тошматов

---
+++ Classes/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace TaskManager_Toshmatov.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventsArgs(prop));
+            }
+        }
+    }
+}
---
+++ Classes/RealyCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace TaskManager_Toshmatov.Classes
+{
+    public class RealyCommand : ICommand
+
+    {
+        private Action<object> execute;
+
+        private Func<object, bool> canExecute;
+
+        public RealyCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested += value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.CanExecute(parameter);
+        }
+        public void Execute(object parameter) =>
+            this.Execute(parameter);
+    }
+}
