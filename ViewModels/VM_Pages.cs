d6576eaa2d122741736811c1c8fd92e72b8434a0
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-15 12:45:49 +0500

    Разработанная модель представления главного окна.  Тошматов

--- ViewModels/VM_Pages.cs
+++ ViewModels/VM_Pages.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TaskManager_Toshmatov.Classes;
 
 namespace TaskManager_Toshmatov.ViewModels
 {
-    public class VM_Pages
+    public class VM_Pages : Notification
     {
+        public VM_Tasks vm_Tasks = new VM_Tasks();
+
+        public VM_Pages() {
+            MainWindow.init.frame.Navigate(new Views.Main(vm_Tasks));
+
+        }
+        public RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
 
     }
 }
